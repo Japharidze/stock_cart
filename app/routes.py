dc6776429e39f0bf15830499e54d81b704f53b26
Mazarin <sergi.japharidze@gmail.com>
2021-10-20 03:24:31 +0400

    0

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,16 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', x=app.config['SECRET_KEY']) 
+    return render_template('index.html')
+
+@app.route('/historical_performance')
+def historical_performance():
+    return "LIE"
+
+@app.route('/manage_list')
+def manage_list():
+    return "DO"
+
+@app.route('/current_alerts')
+def current_alerts():
+    return "DONT"
