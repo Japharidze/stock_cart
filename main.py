993b00aef853376eae4a52d307904cd70b218f53
Mazarin <sergi.japharidze@gmail.com>
2021-10-21 12:39:16 +0400

    0

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import os
 from app import app
 
 if __name__ == '__main__':
-    port = int(os.environ('PORT', 5000))
+    port = int(os.environ.get('PORT', 5000))
     print(port)
     app.run(host='0.0.0.0', port=port)
