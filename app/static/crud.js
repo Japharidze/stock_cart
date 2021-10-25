6f548fa04eb43180742b7a61cdcf1d3a41d5ac96
Mazarin <sergi.japharidze@gmail.com>
2021-10-25 22:35:20 +0400

    deletion draft

--- app/static/crud.js
+++ app/static/crud.js
@@ -17,3 +17,16 @@ function add_stock(){
     }).fail(function() {
         alert('ragacas deendzra')
     })}
+
+$('#deleteStocks').click(function() {
+    var rame = $("div.show").prop('id');
+    alert(rame);
+})
+
+$("#check-all").click(function () {
+		if ($("input:checkbox").prop("checked")) {
+			$("input:checkbox[name='row-check']").prop("checked", true);
+		} else {
+			$("input:checkbox[name='row-check']").prop("checked", false);
+		}
+	});
--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'esgamoiyene'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:admin@localhost/stockdb'
-    # SQLALCHEMY_DATABASE_URI = 'sqlite://stockdb.db'
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:admin@localhost/stockdb'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///stockdb.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
