1c8df5e5680221eb1679c6ed5b450b4714dc4a8e
Mazarin <sergi.japharidze@gmail.com>
2021-10-26 16:35:47 +0400

    modal form for manage_list

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, RadioField, SubmitField
+from wtforms import FloatField, IntegerField, RadioField, SubmitField, TextField
 from wtforms.validators import DataRequired
 
 class InvestmentForm(FlaskForm):
     amount = IntegerField('Hypothetical Investment Amount: ', validators=[DataRequired()])
     trade_type = RadioField('Trades directions to include', choices=[('sell', 'Sell'), ('buy', 'Buy'), ('both', 'Both')])
     submit = SubmitField('View result')
+
+class AddStockForm(FlaskForm):
+    market = RadioField('Market', choices=[('nasdaq', 'Nasssdaq'), ('asx', 'ASX')])
+    stock_code = TextField('Stock Code' , validators=[DataRequired()])
+    entry_price = FloatField('Entry Price', validators=[DataRequired()])
+    submit = SubmitField('Add Stock')
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,11 @@
 from datetime import datetime, timedelta
 
-from flask import render_template, request, jsonify
+from flask import render_template, redirect, url_for, request, jsonify
 from pandas import DataFrame
 
 from app import app, db
 from .stock_api import get_alerts, get_list
-from .forms import InvestmentForm
+from .forms import InvestmentForm, AddStockForm
 from .models import Stock
 
 @app.route('/')
@@ -21,11 +21,19 @@ def historical_performance():
         display = ''
     return render_template('historic_performance.html', form=form, display=display)
 
-@app.route('/manage_list')
+@app.route('/manage_list', methods=['GET', 'POST'])
 def manage_list():
+    form = AddStockForm()
+    if request.method == 'POST':
+        print(form.market.data)
+        print(form.stock_code.data)
+        print(form.entry_price.data)
+        print(form.validate())
+        if form.validate_on_submit():
+            print('asdawr')
     asx_list = Stock.query.filter_by(market='asx').all()
     nasdaq_list = Stock.query.filter_by(market='nasdaq').all()
-    return render_template("list.html", data=[asx_list, nasdaq_list])
+    return render_template("list.html", data=[asx_list, nasdaq_list], form=form)
 
 @app.route('/current_alerts')
 def current_alerts():
@@ -38,11 +46,19 @@ def current_alerts():
 def test():
     return render_template('test.html')
 
-@app.route('/add_stock', methods=["POST"])
-def add_stock():
-    new_stock = Stock(market=request.form['market'],
-                    stock_code=request.form['code'],
-                    entry_price=request.form['price'])
-    db.session.add(new_stock)
-    db.session.commit()
-    return jsonify({'text': 'added'})
+# @app.route('/add_stock', methods=["POST"])
+# def cadd_stock():
+    # new_stock = Stock(market=request.form['market'],
+                    # stock_code=request.form['code'],
+                    # entry_price=request.form['price'])
+    # db.session.add(new_stock)
+    # db.session.commit()
+    # return redirect(url_for('manage_list'))
+    # return render_template('list.html')
+    # return jsonify({'text': 'added'})
+
+@app.route('/delete_stocks', methods=['POST'])
+def del_stocks():
+    print('asdaaaaaaaaaaaaaaaa')
+    print(request.form.getlist('row-check'))
+    return render_template('list.html')
