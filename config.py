2013103080b6ac1d723bff70312b5db5d5de59a9
Mazarin <sergi.japharidze@gmail.com>
2021-10-28 00:39:54 +0400

    config for heroku

--- config.py
+++ config.py
@@ -3,6 +3,8 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'esgamoiyene'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:admin@localhost/stockdb'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') \
+        .replace('postgres://', 'postgresql://') \
+        or 'postgresql://postgres:admin@localhost/stockdb'
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///stockdb.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
